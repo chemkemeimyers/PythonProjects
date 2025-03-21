fd83e604ab0c36536cc26cb4060674747a971361
chemkemeimyers <kemei8@gmail.com>
2025-03-21 08:23:04 -0700

    Upload jupyter notebook on numpy functions

--- PracticingNumpy.py
+++ PracticingNumpy.py
@@ -17,3 +17,17 @@ np_arr_num = np.array(arr_num)
 #Check the output
 print("Numpy Array (arr_str)", np_arr_str)
 print("Numpy Array (arr_num): ", np_arr_num)
+
+#Check the type
+print("Type of arr_str: ", type(arr_str))
+print("Type of arr_num: ", type(arr_num))
+
+print("Type of np_arr_str: ", type(np_arr_str))
+print("Type of np_arr_num: ", type(np_arr_num))
+
+matrix = np.array([[1, 2, 1], [4, 5, 6], [1, 8, 9]])
+print(matrix)
+
+print("Data type of matrix: ", type(matrix))
+
+print(list(range(0,9)))
