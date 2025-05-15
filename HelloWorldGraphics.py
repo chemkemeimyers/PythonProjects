92766b3e7fdcf198d2ed3fc3d095a4669c2c95f1
chemkemeimyers <kemei8@gmail.com>
2025-05-15 05:54:22 -0700

    Graphics Hello World Program

---
+++ HelloWorldGraphics.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=800, height=200)
+    canvas.pack()
+    canvas.create_rectangle(20, 20, 100, 100, fill="blue")
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
