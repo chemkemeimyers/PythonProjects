1e6750f86251ec8e2eafc9b3ae0cb340eea8aa15
Moureen Kemei Myers <kemei8@gmail.com>
2025-06-14 07:14:46 -0700

    updated memory game

--- MemoryGame.py
+++ MemoryGame.py
@@ -7,11 +7,16 @@ NUM_PAIRS = 3
 
 def main():
     listNums = createNumPairsList()
+    #print the original list
     print("The original list: ")
     print(listNums)
+    #Shuffle the numbers
     random.shuffle(listNums)
     print("The shuffled list: ")
     print(listNums)
+
+    #Print the displayedlist
+    displayVisibleList(listNums,[0,0,0,0,0,0])
 
 '''
 Function to create a list with pairs of repeating numbers
@@ -22,6 +27,18 @@ def createNumPairsList():
         for j in range (2):
             num_list.append(i)
     return num_list
+
+'''
+Function to print the displayedlist
+'''
+def displayVisibleList(listNums, guessList):
+    displayList = []
+    for i in range(len(listNums)):
+        displayList.append('*')
+    for i in range(len(listNums)):
+        if (listNums[i] == guessList[i]):
+            displayList[i] = listNums[i]
+    print(displayList)
 
 def clear_terminal():
     for i in range(20):
